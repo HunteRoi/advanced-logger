b12593b09f187c3f359b973d692d9327957fdaad
HunteRoi <tinael.devresse@hotmail.com>
2020-04-24 00:53:19 +0200

    Add built lib & package-log

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,9 @@
+/**
+ * The log event level
+ *
+ * @export
+ * @enum {number}
+ */
 export declare enum LogEventLevel {
     off = 0,
     fatal = 1,
@@ -7,69 +13,80 @@ export declare enum LogEventLevel {
     debug = 31,
     verbose = 63
 }
-interface LoggerOptions {
+/**
+ * The logger options
+ *
+ * @interface ILoggerOptions
+ */
+export interface ILoggerOptions {
     minLevel: LogEventLevel;
     includeTimestamp: boolean;
 }
+/**
+ * The logger class
+ *
+ * @export
+ * @class Logger
+ */
 export declare class Logger {
     private _options;
-    constructor({ minLevel, includeTimestamp }: LoggerOptions);
+    constructor({ minLevel, includeTimestamp }: ILoggerOptions);
     /**
+     * Log a message to the console at the provided `level`. Include the timestamp if `includeTimestamp` is set.
      *
-     *
-     * @private
-     * @param {string} message
-     * @param {LogEventLevel} level
-     * @param {boolean} [includeTimestamp]
+     * @param {string} message the message to log
+     * @param {LogEventLevel} level the level to log the message at
+     * @param {boolean} [includeTimestamp] whether to log the message with the timestamp or not. Defaults to false.
      * @memberof Logger
      */
     log(message: string, level: LogEventLevel, includeTimestamp?: boolean): void;
     /**
+     * Log a message to the console based on the provided `localOptions`
      *
-     *
-     * @private
-     * @param {string} message
-     * @param {LoggerOptions} localOptions
+     * @param {string} message the message to log
+     * @param {ILoggerOptions} localOptions - the local options which are used by the logger
      * @memberof Logger
      */
-    log(message: string, localOptions: LoggerOptions): void;
+    log(message: string, localOptions: ILoggerOptions): void;
     /**
-     *
+     * Logs a fatal error to the console.
+     * When it occurs, it is good practice to kill the process afterwards.
      *
      * @param {string} message
      * @memberof Logger
      */
     fatal: (message: string) => void;
     /**
-     *
+     * Logs an error to the console
      *
      * @param {string} message
      * @memberof Logger
      */
     error: (message: string) => void;
     /**
-     *
+     * Logs a warning to the console
      *
      * @param {string} message
      * @memberof Logger
      */
     warn: (message: string) => void;
     /**
-     *
+     * Logs an information to the console
      *
      * @param {string} message
      * @memberof Logger
      */
     info: (message: string) => void;
     /**
-     *
+     * Logs debug information to the console.
      *
      * @param {string} message
      * @memberof Logger
      */
     debug: (message: string) => void;
     /**
-     *
+     * Logs everything in the console.
+     * Called verbose because it can be quite spammy.
      *
      * @param {string} message
      * @memberof Logger
--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var chalk_1 = __importDefault(require("chalk"));
 var moment_1 = __importDefault(require("moment"));
+/**
+ * The log event level
+ *
+ * @export
+ * @enum {number}
+ */
 var LogEventLevel;
 (function (LogEventLevel) {
     LogEventLevel[LogEventLevel["off"] = 0] = "off";
@@ -15,47 +21,55 @@ var LogEventLevel;
     LogEventLevel[LogEventLevel["debug"] = 31] = "debug";
     LogEventLevel[LogEventLevel["verbose"] = 63] = "verbose";
 })(LogEventLevel = exports.LogEventLevel || (exports.LogEventLevel = {}));
+/**
+ * The logger class
+ *
+ * @export
+ * @class Logger
+ */
 var Logger = /** @class */ (function () {
     function Logger(_a) {
         var _this = this;
         var _b = _a.minLevel, minLevel = _b === void 0 ? LogEventLevel.info : _b, _c = _a.includeTimestamp, includeTimestamp = _c === void 0 ? false : _c;
         /**
-         *
+         * Logs a fatal error to the console.
+         * When it occurs, it is good practice to kill the process afterwards.
          *
          * @param {string} message
          * @memberof Logger
          */
         this.fatal = function (message) { return _this.log(message, LogEventLevel.fatal); };
         /**
-         *
+         * Logs an error to the console
          *
          * @param {string} message
          * @memberof Logger
          */
         this.error = function (message) { return _this.log(message, LogEventLevel.error); };
         /**
-         *
+         * Logs a warning to the console
          *
          * @param {string} message
          * @memberof Logger
          */
         this.warn = function (message) { return _this.log(message, LogEventLevel.warning); };
         /**
-         *
+         * Logs an information to the console
          *
          * @param {string} message
          * @memberof Logger
          */
         this.info = function (message) { return _this.log(message, LogEventLevel.info); };
         /**
-         *
+         * Logs debug information to the console.
          *
          * @param {string} message
          * @memberof Logger
          */
         this.debug = function (message) { return _this.log(message, LogEventLevel.debug); };
         /**
-         *
+         * Logs everything in the console.
+         * Called verbose because it can be quite spammy.
          *
          * @param {string} message
          * @memberof Logger
@@ -69,6 +83,7 @@ var Logger = /** @class */ (function () {
     Logger.prototype.log = function (message, level, includeTimestamp) {
         if (typeof level !== 'number') {
             includeTimestamp = level.includeTimestamp;
+            level = level.minLevel;
         }
         if (this._options.minLevel === LogEventLevel.off || level > this._options.minLevel)
             return;
